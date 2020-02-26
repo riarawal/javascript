5a1e279727240faf5bdac7f2d1aaebe241455987
riarawal <54265567+riarawal@users.noreply.github.com>
2020-02-26 09:41:50 +0530

    Add files via upload

---
+++ ae.js
@@ -0,0 +1,45 @@
+let arr=[2,6,17,28,46,68];
+function map11(arr,ov)
+{
+    for(let i=0;i<arr.length;i++)
+    {
+       arr[i]=ov(arr[i]);
+    }
+}
+
+function ov(ele)
+{
+    if(ele%2==0)
+    return ele+1;
+    else
+    return ele-1;
+}
+let myarr=[];
+function map12(arr,prime)
+{
+    for(let i=0;i<arr.length;i++)
+    { if(prime(arr[i])!=false)
+       myarr.push(prime(arr[i]));
+    }
+}
+function prime(num)
+{
+    var f=0;
+    for(let i=2;i*i<=num;i++)
+    {
+        if(num%i==0)
+        {
+         f=1;
+         return false;
+        }
+    }
+    if(f==0){
+        return num;
+    }
+
+}
+map11(arr,ov);
+map12(arr,prime);
+console.log(myarr);
+
+
