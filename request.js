a9fb6e3d323054c2918826c2ba61932d7b22ec82
riarawal <54265567+riarawal@users.noreply.github.com>
2020-02-26 09:42:19 +0530

    Add files via upload

---
+++ request.js
@@ -0,0 +1,26 @@
+var {exec}=require("child_process")
+const opn = require('opn');
+function takeRequest(data,success,failure){
+    if(data%2==0)
+    {
+        success();
+    }
+    else{
+        failure();
+    }
+}
+
+function success()
+{
+    console.log("completed");
+    exec("calc");
+    
+}
+function failure()
+{
+    console.log("not completed");
+    opn('http://facebook.com', {app: ['Chrome']});
+    
+}
+
+takeRequest(17,success,failure);
