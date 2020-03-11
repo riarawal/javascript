003dbed667989195f9a79ff21065188112c234de
riarawal <54265567+riarawal@users.noreply.github.com>
2020-03-11 21:23:55 +0530

    Add files via upload

---
+++ tree.js
@@ -0,0 +1,20 @@
+let root = {
+    data: 10,
+    children: [
+        { data: 20, children: [{ data: 50, children: [] }, { data: 60, children: [] }] },
+        { data: 30, children: [{ data: 70, children: [{data:130,children:[]}] }, { data: 80, children: [] }] },
+        { data: 40, children: [{ data: 90, children: [] }, { data: 100, children: [] }] }
+    ]
+};
+function displayTree(root) {
+    let str = " " + root.data + "=>";
+    for (let i = 0; i < root.children.length; i++) {
+        str += root.children[i].data +",";
+    }
+    console.log(str);
+    for (let i = 0; i < root.children.length; i++) {
+        displayTree(root.children[i]);
+    }
+
+}
+displayTree(root)
